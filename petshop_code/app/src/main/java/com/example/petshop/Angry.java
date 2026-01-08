96807b633a66e41c0965ffd07b69eca70786c4af
habeeb104 <habeebyusuf683@gmail.com>
2026-01-08 01:38:12 -0700

    Pet Class

---
+++ petshop_code/app/src/main/java/com/example/petshop/Angry.java
@@ -0,0 +1,13 @@
+package com.example.petshop;
+
+import java.util.Date;
+
+public class Angry extends Mood {
+    public Angry(Date date) {
+        super(date);
+    }
+    public Angry() {
+        super();
+    }
+
+}
---
+++ petshop_code/app/src/main/java/com/example/petshop/Happy.java
@@ -0,0 +1,12 @@
+package com.example.petshop;
+
+import java.util.Date;
+
+public class Happy extends Mood {
+    public Happy(Date date) {
+        super(date);
+    }
+    public Happy() {
+        super();
+    }
+}
